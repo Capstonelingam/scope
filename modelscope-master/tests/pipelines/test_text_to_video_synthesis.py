001b012831233c418319ea7308d2c74497a1d617
Ayush Singh <theayushsingh72@gmail.com>
2023-07-27 19:53:45 +0530

    Added reference Files

---
+++ modelscope-master/tests/pipelines/test_text_to_video_synthesis.py
@@ -0,0 +1,31 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+import unittest
+
+from modelscope.outputs import OutputKeys
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+from modelscope.utils.test_utils import test_level
+
+
+class TextToVideoSynthesisTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.task = Tasks.text_to_video_synthesis
+        self.model_id = 'damo/text-to-video-synthesis'
+
+    test_text = {
+        'text': 'A panda eating bamboo on a rock.',
+    }
+
+    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    def test_run_with_model_from_modelhub(self):
+        pipe_line_text_to_video_synthesis = pipeline(
+            task=self.task, model=self.model_id)
+        output_video_path = pipe_line_text_to_video_synthesis(
+            self.test_text)[OutputKeys.OUTPUT_VIDEO]
+        print(output_video_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
